5187888e0f71cc6064a3e76d1af51691b0b1b9cc
Victor Inácio <chustrupgamer@gmail.com>
2023-09-18 10:41:40 -0300

    Can now apply padding to Label Component

--- instagram-clone/components/Label.swift
+++ instagram-clone/components/Label.swift
@@ -2,16 +2,44 @@ import UIKit
 
 class Label: UILabel {
     
+    var insets: UIEdgeInsets?
+    
     func setupView(text: String = "") {
         translatesAutoresizingMaskIntoConstraints = false
         
         self.text = text
     }
     
-    init(text: String = "") {
+    override func drawText(in rect: CGRect) {
+        if let insets = self.insets {
+            super.drawText(in: rect.inset(by: insets))
+        } else {
+            super.drawText(in: rect)
+        }
+        
+       
+        
+        
+    }
+    
+    init(text: String = "", padding: UIEdgeInsets? = nil) {
         super.init(frame: .init(x: 0, y: 0, width: 100, height: 100))
         
+        self.insets = padding
+        
         self.setupView(text: text)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        
+        if let insets = self.insets {
+            return CGSize(width: size.width + insets.left + insets.right,
+                          height: size.height + insets.top + insets.bottom)
+        }
+        
+        return size
+       
     }
     
     override init(frame: CGRect) {
